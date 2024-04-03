6fd0523b05786bd6e41e95eea2639ffdf1adc86e
Sumit <sumitkumarsingh408@gmail.com>
2024-04-03 21:27:46 +0530

    SIP Calculator

--- src/SIPCalculator.java
+++ src/SIPCalculator.java
@@ -1,2 +1,13 @@
 public class SIPCalculator {
+    public static double calculate(int amount, int noOfSIP, double rate, int time) {
+        double totalAmount = 0;
+        double monthlyRate = rate/100/noOfSIP;
+        int totalPeriods = time * noOfSIP;
+
+        for(int i=0; i < totalPeriods; i++) {
+            totalAmount += amount;
+            totalAmount *= (1 + monthlyRate);
+        }
+        return totalAmount;
+    }
 }
